654a1bb7e9e02f66469ac98d7a319a85ae94bdbb
lomari-ship-it <lomari@rampoint.info>
2026-06-24 07:26:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import LateRegistrations from './components/LateRegistrations'
 import KitaiTransfers from './components/KitaiTransfers'
 import LevyList from './components/LevyList'
 import NamibianWagyuSociety from './components/NamibianWagyuSociety'
-import NSBA from './components/NSBA'
 import Namlits from './components/Namlits'
 
 const TABS = [
@@ -84,7 +83,6 @@ export default function App() {
         {tab === 'reconciliation' && <Reconciliation search={search} onSearchChange={setSearch} />}
         {tab === 'levy' && <LevyList search={search} onSearchChange={setSearch} />}
         {tab === 'nws' && <NamibianWagyuSociety />}
-        {tab === 'nsba' && <NSBA />}
         {tab === 'namlits' && <Namlits />}
       </main>
     </div>
